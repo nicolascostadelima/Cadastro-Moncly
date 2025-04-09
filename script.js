b9ac575839597121f35a4236e181aff63765da3c
Nicolas Costa de Lima <nicolasdev4730@gmail.com>
2025-04-08 22:10:39 -0300

    Add js
    add JavaScipt

---
+++ script.js
@@ -0,0 +1,12 @@
+function Validar(){
+    const usuario = document.getElementById("Usuario").value.trim();
+    const senha = document.getElementById("Senha").value.trim();
+    const genero = document.getElementById("Genero").value.trim();
+
+    if (senha === "" || usuario === "" || genero === ""){
+        window.alert("Digite os campos faltentes!");
+    }
+    else{
+        window.location.href = "./registro completo/index.html";
+    }
+}
